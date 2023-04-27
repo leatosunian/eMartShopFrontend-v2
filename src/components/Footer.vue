173cdd2998660a2f91b4c1632df6432b89302d95
Lea <leandrotosunian@hotmail.com>
2023-04-27 12:19:10 -0300

    initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <footer class="main-footer">
+            
+            <!-- Main block - menus, subscribe form-->
+            <div class="text-muted borde" style="background-color: #121212; padding-top: 2.4rem;"> 
+              <div class="container">
+                <div class="row">
+
+                  <div class="mb-5 col-lg-4 mb-lg-0">
+                    <div class="mb-3 text-lg fw-bold text-uppercase text-dark">
+                      <img src="@/assets/emart0000.png" style="width:130px;" alt="eMart brand logo">
+                      <!-- <img src="@/assets/new-deco.png" class="logoImg" style="width:47px!important;" alt=""> -->
+                    </div>
+                    <p>Plataforma de E-commerce</p>
+                    <!-- <p>New Deco - Tienda online</p> -->
+                    <ul class="list-inline">
+                      <li class="list-inline-item"><a class="text-muted text-primary-hover" href="#" target="_blank" title="twitter"><img src="/assets/ig.png" alt="" style="width:26px"></a></li>
+                      <li class="list-inline-item"><a class="text-muted text-primary-hover" href="#" target="_blank" title="twitter"><img src="/assets/tw.png" alt="" style="width:25px"></a></li>
+                      <li class="list-inline-item"><a class="text-muted text-primary-hover" href="#" target="_blank" title="twitter"><img src="/assets/fb.png" alt="" style="width:24px"></a></li>
+                      
+                    </ul>
+                  </div>
+                  
+                  <div class="mb-3 col-lg-2 col-md-6 mb-lg-0">
+                    <h6 class="mb-3 text-white text-uppercase">Shop</h6>
+                    <ul class="list-unstyled">
+                      <li> <a class="text-white" href="#">For Women</a></li>
+                      <li> <a class="text-white" href="#">For Men</a></li>
+                      <li> <a class="text-white" href="#">Stores</a></li>
+                      <li> <a class="text-white" href="#">Our Blog</a></li>
+                      <li> <a class="text-white" href="#">Shop</a></li>
+                    </ul>
+                  </div>
+                  
+                  <div class="mb-3 col-lg-2 col-md-6 mb-lg-0">
+                    <h6 class="mb-3 text-white text-uppercase">Company</h6>
+                    <ul class="list-unstyled">
+                      <li> <a class="text-white" href="#">Login</a></li>
+                      <li> <a class="text-white" href="#">Register</a></li>
+                      <li> <a class="text-white" href="#">Wishlist</a></li>
+                      <li> <a class="text-white" href="#">Our Products</a></li>
+                      <li> <a class="text-white" href="#">Checkouts</a></li>
+                    </ul>
+                  </div>
+                  
+                  <div class="col-lg-4">
+                    <h6 class="mb-3 text-white text-uppercase">Daily Offers & Discounts</h6>
+                    <p class="mb-3 text-white"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. At itaque temporibus.</p>
+                    <h6 class="mb-3 text-white text-uppercase">At itaque temporibus</h6>
+                    <p class="mb-3 text-white"> Lorem ipsum dolor sit amet, consectetur, at itaque temporibus.</p>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <!-- Copyright section of the footer-->
+            <div class="text-white fw-light" style="background-color: #121212; padding-bottom:1.2rem; padding-top: .4rem;">
+              <div class="container">
+                <div class="row align-items-center">
+                  <div class="text-center col-md-6 text-md-start">
+                    <p class="mb-md-0">&copy; Todos los derechos reservados. Desarrollado por <a style="color:orange" target='_blank' href="https://tosunian.dev">tosunian.dev</a> </p>
+                  </div>
+                  <div class="col-md-6">
+                    <ul class="mt-2 mb-0 text-center list-inline mt-md-0 text-md-end">
+                      <li class="list-inline-item"><img class="w-2rem" src="https://d19m59y37dris4.cloudfront.net/sell/2-0/img/visa.svg" alt="..."></li>
+                      <li class="list-inline-item"><img class="w-2rem" src="https://d19m59y37dris4.cloudfront.net/sell/2-0/img/mastercard.svg" alt="..."></li>
+                      <li class="list-inline-item"><img class="w-2rem" src="https://d19m59y37dris4.cloudfront.net/sell/2-0/img/paypal.svg" alt="..."></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+}
+</script>
+  
+  
---
+++ src/views/VerificationStatus.vue
@@ -0,0 +1,204 @@
+<template>
+    <div >
+        <section class="hero productMargin">
+            <div class="container">
+            <!-- Breadcrumbs -->
+            <ol class="breadcrumb justify-content-center">
+                <li class="breadcrumb-item"><router-link to="/">Inicio</router-link></li>
+                <li class="breadcrumb-item active">Estado de compra</li>
+            </ol>
+            
+            </div>
+        </section>
+        <div style="display:flex; flex-direction:column; justify-content:center; align-items:center; gap:30px; margin-top:40px; margin-bottom:50vh;">
+            <img :src="status.icon" alt="icon" style="width: 100px;height: 100px;" v-if="isAddress">
+            <h1 v-if="isAddress">{{ status.title }}</h1>
+            <h1 v-if="!isAddress">{{ submsm_error }} </h1>
+            <h2 v-if="!isAddress">{{ msm_error }} </h2>
+            <p style="font-size:20px" v-if="isAddress">{{ status.subtitle }} </p>
+            <template v-if="validSale">
+                <div>
+                    <span style="font-weight:500; font-size:18px">Redirigiendo al detalle de compra</span>
+
+                    <div class="row">
+                        <div class="text-center col-12">
+                            <div class="mt-4 mb-5 spinner-border" role="status">
+                            <span class="visually-hidden">Cargando...</span>
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+            </template>
+            <router-link v-if="!validSale" to="/" class="btnAdd" style="padding: 11px 26px !important;" >Volver al inicio</router-link>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'VerificationStatus',
+    data(){
+        return {
+            status: {
+                title: '',
+                subtitle: '',
+                icon: ''
+            },
+            payment_id: '',
+            payment: {},
+            address: '',    
+            msm_error: '', 
+            submsm_error: '',
+            isAddress: true,
+            sale: {},
+            saleDetail: [],
+            total: 0,
+            cart: [],
+            validSale: true
+        }
+    },
+    mounted(){
+        window.scrollTo(0, 0)
+    },
+    beforeMount(){
+        this.payment_id = this.$route.query.payment_id 
+        this.sale.transaction = this.payment_id 
+
+        this.getCart()
+        this.getPayment(this.payment_id)
+
+        if(this.$route.params.address){
+            this.address = this.$route.params.address
+            this.sale.address = this.address
+            this.isAddress = true
+        } else{
+            this.isAddress = false
+            this.msm_error = 'No se obtuvo un domicilio de envío'
+            this.submsm_error = 'Error'
+        }
+        
+        if(this.$route.params.status == 'success'){
+            this.status.title = "Pago aprobado"
+            this.status.subtitle = '¡El pago ha sido efectuado correctamente!'
+            this.status.icon = '/assets/icons/checked.png'
+        }
+        if(this.$route.params.status === 'pending'){
+            this.status.title = 'Pendiente de pago'
+            this.status.subtitle = 'Mercado Pago está procesando tu pago. No te preocupes, en unas horas te avisaremos por e-mail si se acreditó.'
+            this.status.icon = '/assets/icons/pending.png'
+            this.validSale = false
+            this.isAddress = true
+        }
+        if(this.$route.params.status === 'failure'){
+            this.status.title = "Pago rechazado"
+            this.status.subtitle = 'El pago no pudo ser efectuado.'
+            this.status.icon = '/assets/icons/cancel.png'
+            this.validSale = false
+            this.isAddress = true
+        }
+        
+     },
+    methods: {
+        getPayment(payment_id){
+            this.payment_id
+            axios.get('https://api.mercadopago.com/v1/payments/'+payment_id, {
+                headers: {
+                    "Content-Type": 'application/json',
+                    "Authorization" : `Bearer APP_USR-8345754408126296-011013-ee93ef49ba895d5a6ca77a7907cd8b50-172136330`
+                }
+            }).then((response) => {
+              const {data} = response
+              this.payment = data
+              if(this.payment.status === 'approved'){
+                this.validateSale(this.payment_id)
+              }
+            }).catch( error => {
+              console.log(error.response.data.msg)
+            }) 
+        },
+
+        validateSale(payment_id){
+            const token = localStorage.getItem('token_shopuser')
+            axios.get(this.$url+'/sales/get/'+payment_id, {
+                headers: {
+                    "Content-Type": 'application/json',
+                    "Authorization" : `Bearer ${token}`
+                }
+            }).then((response) => {
+              const {data} = response
+              if(data.length >= 1){
+                return // no se hace la venta //
+              }
+              if(data.length === 0){
+                setTimeout(() => {
+                    this.createSale()
+                }, 1000);
+              }
+            }).catch( error => {
+              console.log(error.response.data.msg)
+            }) 
+        },
+
+        getCart(){
+            const token = localStorage.getItem('token_shopuser')
+            const getUser = JSON.parse(localStorage.getItem('data_shopuser'))
+            const user = getUser[1]
+            axios.get(this.$url+'/cart/get/'+user, {
+                headers: {
+                    "Content-Type": 'application/json',
+                    "Authorization": `Bearer ${token}`
+                }
+            }).then((response) => {
+                this.total = 0
+                const {data} = response
+                this.cart = data
+
+                for(const item of data){
+                    const subtotal = item.product.price * item.amountOfProducts
+                    this.total = this.total + subtotal
+
+                    this.saleDetail.push({
+                        subtotal: subtotal,
+                        unitPrice: item.product.price,
+                        items: item.amountOfProducts,
+                        client: user,
+                        product: item.product._id,
+                        variant: item.variant._id
+                    })
+                }
+                this.sale.status = this.payment.status
+                this.sale.total = this.total
+                this.sale.client = user
+               
+            }).catch( error => {
+                console.log(error.response.data.msg)
+            })  
+        },
+
+        createSale(){
+            this.sale.shippingPrice = this.$route.params.shippingCost
+            this.sale.shipMethod = this.$route.params.shipMethod
+            this.sale.saleDetail = this.saleDetail
+            const token = localStorage.getItem('token_shopuser')
+            axios.post(this.$url+'/sales/save', this.sale, {
+                headers: {
+                    "Content-Type": 'application/json',
+                    "Authorization" : `Bearer ${token}`
+                }
+            }).then((response) => {
+              const {data} = response
+              this.$socket.emit('sendCart', true)
+              this.validSale = true
+              setTimeout(() => {
+                this.$router.push({name: 'order', params: {id:data._id}})
+              }, 2000);
+            }).catch( error => {
+              console.log(error.response.data.msg)
+            }) 
+        }
+    }
+}
+</script>
