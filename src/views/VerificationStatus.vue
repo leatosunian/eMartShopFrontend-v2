0f0b6d99bc13e5e014d03365f2a1a52c475052d5
Lea <leandrotosunian@hotmail.com>
2023-05-05 14:45:43 -0300

    a

--- src/views/VerificationStatus.vue
+++ src/views/VerificationStatus.vue
@@ -109,6 +109,7 @@ export default {
                 }
             }).then((response) => {
               const {data} = response
+              console.log(data)
               setTimeout(() => {
                 this.$router.push({name: 'order', params: {id:data._id}})
               }, 3000);
@@ -116,7 +117,6 @@ export default {
               console.log(error.response.data.msg)
             }) 
         },
-
     }
 }
 </script>
